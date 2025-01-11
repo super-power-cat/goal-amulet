6ba0b90cda0af42b541499788653ecae13f5bb38
jongeuni <lje0818ovo@naver.com>
2025-01-12 01:00:15 +0900

    부적 내부 크기 조절 및 텍스트 세줄 넘지 않도록 추가

--- src/components/AmuletContainer.tsx
+++ src/components/AmuletContainer.tsx
@@ -1,5 +1,6 @@
 import { ColorKey, getColorInfo } from '../types';
 import styles from './AmuletContainer.module.css';
+import { useState } from 'react';
 
 interface AmuletContainerProps {
   selectedColor: ColorKey;
@@ -9,12 +10,30 @@ interface AmuletContainerProps {
 
 export const AmuletContainer = ({ selectedColor, text, onTextChange }: AmuletContainerProps) => {
   const colorInfo = getColorInfo(selectedColor);
+  const [showWarning, setShowWarning] = useState(false);
 
+
+  // 이럴 경우 쭉 입력했을때 줄바꿈 되는 걸 방어 못함
   const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    if (onTextChange) {
-      onTextChange(e.target.value);
+    const lines = e.target.value.split('\n');
+    
+    if (lines.length > 3 || !onTextChange) {
+      e.preventDefault();
+      setShowWarning(true);
+      // 3줄까지만 허용
+      
+      // 경고 메시지 3초 후 사라지게 설정
+      setTimeout(() => {
+        setShowWarning(false);
+      }, 3000);
+      return;
+    } else {    
+        const truncatedText = lines.slice(0, 3).join('\n');
+        onTextChange(truncatedText);
+        setShowWarning(false);
     }
   };
+  
 
   return (
     <div 
@@ -35,6 +54,11 @@ export const AmuletContainer = ({ selectedColor, text, onTextChange }: AmuletCon
       <textarea className={styles.amuletText} onChange={handleTextChange} 
                 rows={3} maxLength={100} value={text}>
       </textarea>
+      {showWarning && (
+        <div className={`${styles.warningMessage} ${styles.visible}`}>
+          최대 3줄까지만 입력할 수 있습니다 ⚠️
+        </div>
+      )}
       
       {/* <textarea 
         value={text}
